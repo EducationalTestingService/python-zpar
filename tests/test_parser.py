df3a8ba82a109d10e72bd6cf11ff0dc824489872
Nitin Madnani <nmadnani@ets.org>
2016-06-18 00:04:25 -0400

    Streamline constituency parser tests.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -8,6 +8,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from io import open
+from itertools import product
 from os.path import abspath, dirname, join
 
 from nose.tools import assert_equal
@@ -15,32 +16,52 @@ from nose.tools import assert_equal
 _my_dir = abspath(dirname(__file__))
 
 
-def check_parse_sentence(tokenize=False):
+def check_parse_sentence(tokenize=False, tagged=False):
     """
     Check parse_sentence method with and without tokenization
+    and with and without pre-tagged output.
     """
     from tests import parser
 
-    sentence = "I'm going to the market." if tokenize else "I 'm going to the market ."
+
+    if tagged:
+        sentence = "I/PRP 'm/VBP going/VBG to/TO the/DT market/NN ./."
+    else:
+        if tokenize:
+            sentence = "I'm going to the market."
+        else:
+            sentence = "I 'm going to the market ."
+
     correct_output = "(S (NP (PRP I)) (VP (VBP 'm) (VP (VBG going) (PP (TO to) (NP (DT the) (NN market))))) (. .))"
-    parsed_sentence = parser.parse_sentence(sentence, tokenize=tokenize)
+
+    if not tagged:
+        parsed_sentence = parser.parse_sentence(sentence, tokenize=tokenize)
+    else:
+        parsed_sentence = parser.parse_tagged_sentence(sentence)
 
     assert_equal(parsed_sentence, correct_output)
 
 
 def test_parse_sentence():
-    yield check_parse_sentence, False
-    yield check_parse_sentence, True
+    for (tokenize, tagged) in product([True, False], [True, False]):
+        yield check_parse_sentence, tokenize, tagged
 
 
-def check_parse_file(tokenize=False):
+def check_parse_file(tokenize=False, tagged=False):
     """
     Check parse_file method with and without tokenization
+    and with and without pre-tagged output
     """
 
     from tests import parser
 
-    prefix = 'test' if tokenize else 'test_tokenized'
+    if tagged:
+        prefix = 'test_tagged'
+    else:
+        if tokenize:
+            prefix = 'test'
+        else:
+            prefix = 'test_tokenized'
 
     correct_output = ["(S (NP (PRP I)) (VP (VBP am) (VP (VBG going) (PP (TO to) (NP (DT the) (NN market))))) (. .))",
                       "(SQ (VBP Are) (NP (PRP you)) (VP (VBG going) (S (VP (TO to) (VP (VB come) (PP (IN with) (NP (PRP me))))))) (. ?))"]
@@ -49,7 +70,10 @@ def check_parse_file(tokenize=False):
     output_file = abspath(join(_my_dir, '..', 'examples', '{}.parse'.format(prefix)))
 
     # parse the file
-    parser.parse_file(input_file, output_file, tokenize=tokenize)
+    if not tagged:
+        parser.parse_file(input_file, output_file, tokenize=tokenize)
+    else:
+        parser.parse_tagged_file(input_file, output_file)
 
     # read the output file and make sure we have the expected output
     with open(output_file, 'r') as outf:
@@ -59,35 +83,6 @@ def check_parse_file(tokenize=False):
 
 
 def test_parse_file():
-    yield check_parse_file, False
-    yield check_parse_file, True
+    for (tokenize, tagged) in product([True, False], [True, False]):
+        yield check_parse_file, tokenize, tagged
 
-
-def test_parse_tagged_sentence():
-    from tests import parser
-
-    tagged_sentence = "I/PRP 'm/VBP going/VBG to/TO the/DT market/NN ./."
-    correct_output = "(S (NP (PRP I)) (VP (VBP 'm) (VP (VBG going) (PP (TO to) (NP (DT the) (NN market))))) (. .))"
-    parsed_sentence = parser.parse_tagged_sentence(tagged_sentence)
-
-    assert_equal(parsed_sentence, correct_output)
-
-
-def test_parse_tagged_file():
-
-    from tests import parser
-
-    correct_output = ["(S (NP (PRP I)) (VP (VBP am) (VP (VBG going) (PP (TO to) (NP (DT the) (NN market))))) (. .))",
-                      "(SQ (VBP Are) (NP (PRP you)) (VP (VBG going) (S (VP (TO to) (VP (VB come) (PP (IN with) (NP (PRP me))))))) (. ?))"]
-
-    input_file = abspath(join(_my_dir, '..', 'examples', 'test_tagged.txt'))
-    output_file = abspath(join(_my_dir, '..', 'examples', 'test_tagged.parse'))
-
-    # parse the file
-    parser.parse_tagged_file(input_file, output_file)
-
-    # read the output file and make sure we have the expected output
-    with open(output_file, 'r') as outf:
-        output = [l.strip() for l in outf.readlines()]
-
-    assert_equal(output, correct_output)
